0962fd24f08693a6fa6af17b29f7f543687b2d30
Ráfaga Queiroz <rafaga.queiroz@gmail.com>
2019-03-27 17:14:13 -0300

    formating data for sending

--- src/SAPBridge.php
+++ src/SAPBridge.php
@@ -39,7 +39,12 @@ class SAPBridge implements BridgeInterface
     {
         switch ($request->getMethodIdentifier()) {
             case 'EnviarCliente':
-                //$this->client->post(self::URI_ENVIAR_CLIENTE, $request->getData());
+                $this->client->post(
+                    self::URI_ENVIAR_CLIENTE,
+                    [
+                        'json' => $this->getClienteData($request->getData())
+                    ]
+                );
                 break;
 
             default:
@@ -54,5 +59,50 @@ class SAPBridge implements BridgeInterface
     public function search(RequestInterface $request)
     {
         return null;
+    }
+
+    /**
+     * @param \stdClass $cliente
+     * @return array
+     */
+    private function getClienteData(\stdClass $cliente)
+    {
+        $dados = [
+            'altkn' => $cliente->codigo,
+            'dados_gerais' => [
+                'bpext' => $cliente->codigo,
+                'kunnr' => '',
+                'name_org' => $cliente->nome,
+                'sort1' => $cliente->documentoNumero,
+                'sort2' => $cliente->nomeTratamento,
+                'street' => $cliente->enderecoLogradouro,
+                'house_num1' => $cliente->enderecoNumero,
+                'post_code1' => $cliente->enderecoCep,
+                'cod_municipio' => $cliente->enderecoCodigoMunicipio,
+                'city2' => $cliente->enderecoBairro,
+                'city1' => $cliente->enderecoCidade,
+                'country' => $cliente->pais,
+                'region' => $cliente->enderecoUf,
+                'telf1' => $cliente->telefone,
+                'telf2' => $cliente->telefone2,
+                'telfx' => '',
+                'smtp_addr' => $cliente->email
+            ],
+            'dados_identificacao' => [
+                [
+                    'taxtype' => $cliente->documentoTipo,
+                    'taxnum' => $cliente->documentoNumero,
+                ]
+            ]
+        ];
+
+        if (false === empty($cliente->inscricaoEstadual)) {
+            $dados['dados_identificacao'][] = [
+                'taxtype' => 'IE',
+                'taxnum' => $cliente->inscricaoEstadual,
+            ];
+        }
+
+        return ['dados' => [$dados]];
     }
 }
